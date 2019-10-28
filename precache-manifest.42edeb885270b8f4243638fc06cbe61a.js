569efc58eb814b35133e4c7ff06b1afba340f745
success <youngyannsi@gmail.com>
2019-10-27 23:28:34 -0700

    Updates

---
+++ precache-manifest.42edeb885270b8f4243638fc06cbe61a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b76c262e200db01f117489b798010c6",
+    "url": "/Aface/index.html"
+  },
+  {
+    "revision": "d66fd78ea871bc01f3e3",
+    "url": "/Aface/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "05185e014b15a22e88a4",
+    "url": "/Aface/static/css/main.69cd9325.chunk.css"
+  },
+  {
+    "revision": "d66fd78ea871bc01f3e3",
+    "url": "/Aface/static/js/2.6d9fd9c1.chunk.js"
+  },
+  {
+    "revision": "05185e014b15a22e88a4",
+    "url": "/Aface/static/js/main.e2f660d8.chunk.js"
+  },
+  {
+    "revision": "4384f1f72b39998d8926",
+    "url": "/Aface/static/js/runtime~main.80bcf50d.js"
+  }
+]);
